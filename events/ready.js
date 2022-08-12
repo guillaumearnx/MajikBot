9cb8b4d01a77dcaecbdce000565010435cd47709
ARNOUX Guillaume <arnouxguillaume54@hotmail.fr>
2022-08-12 03:24:32 +0200

    update from template

--- events/ready.js
+++ events/ready.js
@@ -1,12 +1,17 @@
-const {deployInteractions, loadInteractions, removeIntegrations} = require("../utils/functions");
+const { deployInteractions, loadInteractions, removeIntegrations } = require("../utils/functions");
 
 module.exports = async (bot) => {
-    console.info(`\nLogged in as ${bot.user.tag}!`.blue);
-    bot.interactions = await loadInteractions(bot);
-    if (process.env.DEV)
-        removeIntegrations().then(() => {
-            deployInteractions(!process.env.DEV, bot.interactions)
-        }).catch(e => console.error(e));
-    else
-        deployInteractions(!process.env.DEV, bot.interactions).catch(e => console.error(e));
-}
+	console.info(`\nLogged in as ${bot.user.tag}!`.blue);
+	bot.interactions = await loadInteractions(bot);
+	if (process.env.DEV) {
+		try {
+			await removeIntegrations();
+			await deployInteractions(!process.env.DEV, bot.interactions);
+		}
+		catch (err) {
+			console.error(err);
+			process.exit(533);
+		}
+	}
+	else {deployInteractions(!process.env.DEV, bot.interactions).catch(e => console.error(e));}
+};
